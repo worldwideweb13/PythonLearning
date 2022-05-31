1d4927b869e7122348cc79fbca334981ca0c3b84
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-06-01 04:56:01 +0900

    :card_file_box: Models,migrationファイル追加 #1

--- ToDoApp/todo/models.py
+++ ToDoApp/todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+# class名はキャメルケースで書くことが通例
+# 継承を使う場合 'from hoge import 継承name' で文頭に宣言 
+# 継承をする場合はclass name(継承するmethod)で記述する
+class TodoModel(models.Model):
+    title = models.CharField(max_length=100)
+    memo = models.TextField()
+    # モデルオブジェクトに名前をつける
+    #  __str__(self)...modelオブジェクト自身を文字列として受け取り、titileを表示名に使う
+    def __str__(self):
+        return self.title
