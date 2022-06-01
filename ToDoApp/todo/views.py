ced33eb7ca51c38b54fefc4935fbe9a273ebe225
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-06-02 00:56:58 +0900

    :sparkles: CRUD作成

--- ToDoApp/todo/views.py
+++ ToDoApp/todo/views.py
@@ -1,8 +1,8 @@
-from pydoc import HTMLDoc
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView,DetailView,CreateView,DeleteView,UpdateView
 from .models import TodoModel
+from django.urls import reverse_lazy
+
 
 # Create your views here.
 
@@ -13,3 +13,22 @@ class TodoList(ListView):
 class TodoDetail(DetailView):
     template_name = 'detail.html'
     model = TodoModel
+
+class TodoCreate(CreateView):
+    template_name = 'create.html'
+    model = TodoModel
+    fields = ('title','memo','priority','duedate')
+    # viewsからurlsを呼び出す...通常の流れ(urls-view)と逆なのでreverse
+    # classで指定する場合はreverse_lazy,関数で指定する場合はreverseを利用する
+    success_url = reverse_lazy('list')
+
+class TodoDelete(DeleteView):
+    template_name = 'delete.html'
+    model = TodoModel
+    success_url = reverse_lazy('list')
+
+class TodoUpdate(UpdateView):
+    template_name = 'update.html'
+    model = TodoModel
+    fields = ('title','memo','priority','duedate')   
+    success_url = reverse_lazy('list')
