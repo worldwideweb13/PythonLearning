a2c4384d359f552268337c9a624b34d8b798acd3
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-06-02 21:30:42 +0900

    :sparkles: サインアップ機能実装

---
+++ BoardApp/boardapp/test.py
@@ -0,0 +1,11 @@
+def signupfunc(request):
+    if request.method == 'POST':
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        try:
+            User.objects.get(username=username2)
+            return render(request, 'signup.html', {'error':'このユーザーは登録されています'})
+        except:
+            user = User.objects.create_user(username2, '', password2)
+            return render(request, 'signup.html', {'some':100})
+    return render(request, 'signup.html', {'some':100})
