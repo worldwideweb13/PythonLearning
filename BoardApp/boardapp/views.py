ff84d3be09c533a386e0897dcc6df6b3b558cccd
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-06-02 22:42:44 +0900

    :sparks: model作成& 画像データの取り扱い設定

--- BoardApp/boardapp/views.py
+++ BoardApp/boardapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 
 # Create your views here.
 def signupfunc(request):
@@ -13,3 +15,19 @@ def signupfunc(request):
             user = User.objects.create_user(username,'',password)
             return render(request,'signup.html',{'some':100})
     return render(request, 'signup.html',{'some':100})
+
+def loginfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request,username=username, password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('signup')
+        else:
+            return redirect('login')
+    return render(request,'login.html')
+
+def listfunc(request):
+
+    return render(request,'list.html')
